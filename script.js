cdd2b12a6d91940f32512ec4a4c562dc5098e03a
Glitch (reliable-equatorial-rosehip) <none>
2020-12-02 03:37:19 +0000

    🖖🎗 Updated with Glitch

--- script.js
+++ script.js
@@ -3,3 +3,21 @@
 
 // prints "hi" in the browser's dev tools console
 console.log("hi");
+
+
+document.getElementById("demo").innerHTML = "Hello World!";
+function changeText(id) {
+  id.innerHTML = "BOOO!";
+}
+  function mOver(obj) {
+  obj.innerHTML = "Thank You"
+}
+
+function mOut(obj) {
+  obj.innerHTML = "Mouse Over Me"
+}
+document.getElementById("myBtn").addEventListener("click", displayDate);
+
+function myFunction() {
+  alert("Hello world!");
+}
