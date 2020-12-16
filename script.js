ce175809617aa3a6e2534858ffee82ae5085d932
Glitch (malachite-ionized-harrier) <none>
2020-12-16 01:37:39 +0000

    💶🚃 Updated with Glitch

--- script.js
+++ script.js
@@ -3,21 +3,23 @@
 
 // prints "hi" in the browser's dev tools console
 console.log("hi");
+// Automatic Slideshow - change image every 3 seconds
+var myIndex = 0;
+carousel();
 
-
-document.getElementById("demo").innerHTML = "Hello World!";
-function changeText(id) {
-  id.innerHTML = "BOOO!";
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}
+  x[myIndex-1].style.display = "block";
+  setTimeout(carousel, 3000);
 }
-  function mOver(obj) {
-  obj.innerHTML = "Thank You"
-}
-
-function mOut(obj) {
-  obj.innerHTML = "Mouse Over Me"
-}
-document.getElementById("myBtn").addEventListener("click", displayDate);
 
 function myFunction() {
-  alert("Hello world!");
+  var element = document.body; document.paragraph;
+  element.classList.toggle("dark-mode");
 }
